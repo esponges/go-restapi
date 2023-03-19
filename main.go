00e122c420daa192115b2a300e7d2337df41f35e
esponges <esponges@gmail.com>
2023-03-19 09:34:10 -0600

    test importing fns from other files

--- main.go
+++ main.go
@@ -5,92 +5,94 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"restapi/otherfns"
 
 	"github.com/gorilla/mux"
 )
 
 type Book struct {
-    ID     string  `json:"id,omitempty"`
-    Title  string  `json:"title,omitempty"`
-    Author string  `json:"author,omitempty"`
-    ISBN   string  `json:"isbn,omitempty"`
+	ID     string `json:"id,omitempty"`
+	Title  string `json:"title,omitempty"`
+	Author string `json:"author,omitempty"`
+	ISBN   string `json:"isbn,omitempty"`
 }
 
 var books []Book
 
 func main() {
-    // Initialize router
-    router := mux.NewRouter()
+	// Initialize router
+	router := mux.NewRouter()
 
-    // Add some sample data
-    books = append(books, Book{ID: "1", Title: "Book One", Author: "Author One", ISBN: "12345"})
-    books = append(books, Book{ID: "2", Title: "Book Two", Author: "Author Two", ISBN: "67890"})
+	// Add some sample data
+	books = append(books, Book{ID: "1", Title: "Book One", Author: "Author One", ISBN: "12345"})
+	books = append(books, Book{ID: "2", Title: "Book Two", Author: "Author Two", ISBN: "67890"})
 
-    // Define routes
-    router.HandleFunc("/books", getBooks).Methods("GET")
-    router.HandleFunc("/books/{id}", getBook).Methods("GET")
-    router.HandleFunc("/books", createBook).Methods("POST")
-    router.HandleFunc("/books/{id}", updateBook).Methods("PUT")
-    router.HandleFunc("/books/{id}", deleteBook).Methods("DELETE")
+	// Define routes
+	router.HandleFunc("/books", getBooks).Methods("GET")
+	router.HandleFunc("/books/{id}", getBook).Methods("GET")
+	router.HandleFunc("/books", createBook).Methods("POST")
+	router.HandleFunc("/books/{id}", updateBook).Methods("PUT")
+	router.HandleFunc("/books/{id}", deleteBook).Methods("DELETE")
 
-    // Start server
-    log.Fatal(http.ListenAndServe(":8000", router))
+	// Start server
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
 // Get all books
 func getBooks(w http.ResponseWriter, r *http.Request) {
-    json.NewEncoder(w).Encode(books)
+	otherfns.HelloHandler()
+	json.NewEncoder(w).Encode(books)
 }
 
 // Get single book by ID
 func getBook(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("getBook() called")
+	fmt.Println("getBook() called")
 
-    params := mux.Vars(r)
-    for _, book := range books {
-        if book.ID == params["id"] {
-            json.NewEncoder(w).Encode(book)
-            return
-        }
-    }
-    json.NewEncoder(w).Encode(nil)
+	params := mux.Vars(r)
+	for _, book := range books {
+		if book.ID == params["id"] {
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(nil)
 }
 
 // Create a new book
 func createBook(w http.ResponseWriter, r *http.Request) {
-    var book Book
-    json.NewDecoder(r.Body).Decode(&book)
-    books = append(books, book)
-    json.NewEncoder(w).Encode(book)
+	var book Book
+	json.NewDecoder(r.Body).Decode(&book)
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
 }
 
 // Update an existing book by ID
 func updateBook(w http.ResponseWriter, r *http.Request) {
-    params := mux.Vars(r)
-    for i, book := range books {
-        if book.ID == params["id"] {
-            books[i] = Book{
-                ID:     book.ID,
-                Title:  book.Title,
-                Author: book.Author,
-                ISBN:   book.ISBN,
-            }
-            json.NewDecoder(r.Body).Decode(&books[i])
-            json.NewEncoder(w).Encode(books[i])
-            return
-        }
-    }
-    json.NewEncoder(w).Encode(nil)
+	params := mux.Vars(r)
+	for i, book := range books {
+		if book.ID == params["id"] {
+			books[i] = Book{
+				ID:     book.ID,
+				Title:  book.Title,
+				Author: book.Author,
+				ISBN:   book.ISBN,
+			}
+			json.NewDecoder(r.Body).Decode(&books[i])
+			json.NewEncoder(w).Encode(books[i])
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(nil)
 }
 
 // Delete a book by ID
 func deleteBook(w http.ResponseWriter, r *http.Request) {
-    params := mux.Vars(r)
-    for i, book := range books {
-        if book.ID == params["id"] {
-            books = append(books[:i], books[i+1:]...)
-            break
-        }
-    }
-    json.NewEncoder(w).Encode(books)
+	params := mux.Vars(r)
+	for i, book := range books {
+		if book.ID == params["id"] {
+			books = append(books[:i], books[i+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
---
+++ otherfns/other.go
@@ -0,0 +1,10 @@
+package otherfns
+
+import (
+	"fmt"
+)
+
+// HelloHandler is a function that handles the /hello route
+func HelloHandler() {
+	fmt.Println("Hello, World! from otherfns")
+}
