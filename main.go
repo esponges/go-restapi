0fc1b1a5e3a676d28f4bd88711ac9f3afbe64783
esponges <esponges@gmail.com>
2023-03-18 19:32:32 -0600

    get hello world running

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	router := mux.NewRouter()
+
+	// helo world
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
 }
